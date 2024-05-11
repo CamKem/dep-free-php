c8518e3c6988eae0679f50a213e0592e676b2f1f
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-11 23:31:38 +1000

    feat(builder): new HasMany relationship added

---
+++ app/Core/Database/Relations/HasMany.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\Core\Database\Relations;
+
+use App\Core\Collecting\ModelCollection;
+use app\Core\Database\Model;
+
+class HasMany
+{
+    protected Model $parent;
+    protected Model $related;
+    protected string $foreignKey;
+
+    public function __construct(Model $parent, Model $related)
+    {
+        $this->parent = $parent;
+        $this->related = $related;
+        $this->foreignKey = strtolower(class_basename($parent)) . '_id';
+    }
+
+    public function getRelatedTable(): string
+    {
+        return $this->related->getTable();
+    }
+
+    public function getForeignKey(): string
+    {
+        return $this->foreignKey;
+    }
+
+    public function get(): ModelCollection
+    {
+        return $this->related::where($this->foreignKey, $this->parent->id)->get();
+    }
+
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use app\Core\Database\Model;
+use app\Core\Database\Relations\HasMany;
 
 class Category extends Model
 {
     protected string $table = 'categories';
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 
 }
