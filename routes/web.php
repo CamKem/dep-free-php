24b72e048a1562fe1cd4b8c1be5e0b45f8de04c2
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-11 18:42:36 +1000

    feat(routes): remove obsolete routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Controllers\AboutController;
-use App\Controllers\Auth\SessionController;
 use App\Controllers\CategoriesController;
 use App\Controllers\ContactController;
 use App\Controllers\HomeController;
-use App\Controllers\NotesController;
 use App\Controllers\ProductsController;
 use App\Core\Routing\RouteProxy as Route;
 
@@ -42,39 +40,3 @@ Route::get('/products')
 Route::get('/categories/{category}/products/{product}')
     ->controller([ProductsController::class, 'show'])
     ->name('products.show');
-
-// Login routes
-Route::get('/login')
-    ->controller([SessionController::class, 'index'])
-    ->name('login.index');
-
-// Notes Routes
-Route::get('/notes')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'index'])
-    ->name('notes.index');
-
-Route::get('/notes/create')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'create'])
-    ->name('notes.create');
-
-Route::get('/notes/{note}')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'show'])
-    ->name('notes.show');
-
-Route::delete('/notes/{note}')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'destroy'])
-    ->name('notes.destroy');
-
-Route::post('/notes')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'store'])
-    ->name('notes.store');
-
-Route::get('/notes/{note}/edit')
-    //->middleware('auth')
-    ->controller([NotesController::class, 'edit'])
-    ->name('notes.edit');
