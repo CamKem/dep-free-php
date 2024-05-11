70281943a7aaea1e844015c7f6316d1d7ecb0315
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-11 23:32:20 +1000

    feat(products): product grid component extracted

--- views/layouts/app.view.php
+++ views/layouts/app.view.php
@@ -1,12 +1,12 @@
-<?= add('layouts/partials/head', ['title' => $title]) ?>
-<?= add('layouts/partials/header', ['categories' => session()->get('categories')]) ?>
+<?= add('layouts.partials.head', ['title' => $title]) ?>
+<?= add('layouts.partials.header', ['categories' => session()->get('categories')]) ?>
     <main class="content-container">
 
         <?= $content ?>
 
-        <?= add('layouts/partials/brands') ?>
+        <?= add('layouts.partials.brands') ?>
 
     </main>
-<?= add('layouts/partials/footer', ['categories' => session()->get('categories')]) ?>
-<?= add('layouts/partials/flash') ?>
-<?= add('layouts/partials/bottom') ?>
+<?= add('layouts.partials.footer', ['categories' => session()->get('categories')]) ?>
+<?= add('layouts.partials.flash') ?>
+<?= add('layouts.partials.bottom') ?>
---
+++ views/products/partials/grid.view.php
@@ -0,0 +1,30 @@
+<ul class="product-grid">
+    <?php foreach ($products->toArray() as $product): ?>
+        <li>
+            <a href="<?= route('products.show', [
+                    'category' => $product['category']['slug'],
+                    'product' => $product['slug']]
+            ) ?>"
+               class="product-link"
+               aria-label="<?= $product['name'] ?>"
+            >
+                <article class="product-card">
+                    <img src="./images/products/<?= $product['image'] ?>"
+                         alt="<?= $product['name'] ?>">
+                    <p class="price">
+                        <?php if ($product['sale_price']): ?>
+                            <strong class="price-sale">$<?= $product['sale_price'] ?></strong>
+                            <span class="original-price-group">
+                                    <span class="was-text">WAS</span>
+                                    <del>$<?= $product['price'] ?></del>
+                                </span>
+                        <?php else: ?>
+                            <strong>$<?= $product['price'] ?></strong>
+                        <?php endif; ?>
+                    </p>
+                    <h3 class="short-desc"><?= substr($product['description'], 0, 30) ?>...</h3>
+                </article>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
