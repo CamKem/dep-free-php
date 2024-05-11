20079b9ffc72395dec859ec5e54c81d0aeb93a71
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-11 18:41:12 +1000

    feat(builder): allow for chained methods and different operator

--- app/Core/Database/QueryBuilder.php
+++ app/Core/Database/QueryBuilder.php
@@ -5,6 +5,7 @@ namespace app\Core\Database;
 class QueryBuilder
 {
 
+    protected static self|null $instance = null;
     protected string $query;
     protected array $conditions = [];
     protected array $with = [];
@@ -16,36 +17,45 @@ class QueryBuilder
         $this->query = "select * from $this->table";
     }
 
-    public static function where(string $column, mixed $value): static
+    public static function getInstance(): static
     {
-        return (new static())->addCondition($column, $value);
-       // $this->conditions[] = [$column, $value];
+        return static::$instance ??= new static();
+    }
+
+    public static function where(string $column, mixed $operator, mixed $value = null): static
+    {
+        if (func_num_args() === 2) {
+            $value = $operator;
+            $operator = "=";
+        }
+        $queryBuilder = self::getInstance();
+        $queryBuilder->conditions[] = [$column, $operator, $value];
+        return $queryBuilder;
     }
 
     public static function with(string $relation): static
     {
-        $queryBuilder = new static();
+        $queryBuilder = self::getInstance();
         $queryBuilder->with[] = $relation;
         return $queryBuilder;
     }
 
-    public function addCondition(string $column, mixed $value): static
-    {
-        $this->conditions[] = [$column, $value];
-        return $this;
-    }
-
     public function toSql(): string
     {
-        if (!empty($this->conditions)) {
-            $this->query .= " where ";
-            foreach ($this->conditions as $index => $condition) {
-                if ($index > 0) {
-                    $this->query .= " and ";
-                }
-                $this->query .= "{$condition[0]} = :{$condition[0]}";
-            }
-        }
+        $baseQuery = $this->query;
+
+//        if (!empty($this->conditions)) {
+//            $baseQuery .= " where ";
+//            //$this->query .= " where ";
+//            foreach ($this->conditions as $index => $condition) {
+//                if ($index > 0) {
+//                    $baseQuery .= " and ";
+//                    //$this->query .= " and ";
+//                }
+//                $baseQuery .= "{$condition[0]} = :{$condition[0]}";
+//                //$this->query .= "{$condition[0]} = :{$condition[0]}";
+//            }
+//        }
 
         if (!empty($this->with)) {
             $modelInstance = new static();
@@ -57,23 +67,46 @@ class QueryBuilder
                 // work out the column names
                 $columns = $this->db->execute("SHOW COLUMNS FROM {$relatedTable}")->get();
 
-                $prefixedColumns = array_map(static function ($column) use ($relatedTable) {
-                    return "{$relatedTable}.{$column['Field']} as {$relatedTable}_{$column['Field']}";
+                $prefixedColumns = array_map(static function ($column) use ($relatedTable, $relation) {
+                    return "{$relatedTable}.{$column['Field']} as {$relation}_{$column['Field']}";
                 }, $columns);
 
-                $this->query = str_replace("select *", "select {$this->table}.*, " . implode(", ", $prefixedColumns), $this->query);
-                $this->query .= " left join {$relatedTable} on {$this->table}.{$foreignColumn} = {$relatedTable}.id";
+                $baseQuery = str_replace("select *", "select {$this->table}.*, " . implode(", ", $prefixedColumns), $baseQuery);
+                $baseQuery .= " inner join {$relatedTable} on {$this->table}.{$foreignColumn} = {$relatedTable}.id";
+                //$this->query = str_replace("select *", "select {$this->table}.*, " . implode(", ", $prefixedColumns), $this->query);
+                //$this->query .= " left join {$relatedTable} on {$this->table}.{$foreignColumn} = {$relatedTable}.id";
             }
         }
 
-        return $this->query;
+        if (!empty($this->conditions)) {
+            $baseQuery .= " where ";
+            //$this->query .= " where ";
+            foreach ($this->conditions as $index => $condition) {
+                if ($index > 0) {
+                    $baseQuery .= " and ";
+                    //$this->query .= " and ";
+                }
+                // if there is a with, we need to prefix the column name
+                if (!empty($this->with)) {
+                    $baseQuery .= "{$this->table}.{$condition[0]} {$condition[1]} :{$condition[0]}";
+                    //$this->query .= "{$this->table}.{$condition[0]} = :{$condition[0]}";
+                } else {
+                    $baseQuery .= "{$condition[0]} {$condition[1]} :{$condition[0]}";
+                    //$this->query .= "{$condition[0]} = :{$condition[0]}";
+                }
+            }
+        }
+
+        return $baseQuery;
     }
 
     public function getBindings(): array
     {
-        return array_combine(array_map(static function ($condition) {
-            return $condition[0];
-        }, $this->conditions), array_column($this->conditions, 1));
+        $bindings = [];
+        foreach ($this->conditions as $condition) {
+            $bindings[$condition[0]] = $condition[2];
+        }
+        return $bindings;
     }
 
 }
