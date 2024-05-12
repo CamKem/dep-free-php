e93a6f0f5552a95ace5c3dba9e19013e6b2dd761
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-12 11:49:55 +1000

    feat(categories): abort if there is no valid category

--- app/Controllers/CategoriesController.php
+++ app/Controllers/CategoriesController.php
@@ -13,6 +13,11 @@ class CategoriesController extends Controller
     public function show(Request $request): View
     {
         $category = Category::where('slug', $request->get('category'))->first();
+
+        if (!$category) {
+            abort();
+        }
+
         return view("categories.show", [
             'title' => $category->name,
             'category' => $category,
