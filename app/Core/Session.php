e4c50d72b33f7283deef3ed4a7fa223b61b2b69a
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-12 20:48:05 +1000

    feat(session): fixed the ability to flash data for 1 request cycle to the session

--- app/Core/Session.php
+++ app/Core/Session.php
@@ -4,31 +4,47 @@ namespace App\Core;
 
 class Session
 {
+
+    protected ?string $id = null;
+    protected array $data = [];
+
     public function __construct()
     {
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
+
+        $this->id = session_id();
+        $this->migrateDataForCurrentRequest();
     }
 
-    public function get(string $key, $default = null)
+    public function get(string $key, $default = null): mixed
     {
-        return $_SESSION[$key] ?? $default;
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+        // check in the flash data
+        $flashData = array_merge($this->data['_flash.old'] ?? [], $this->data['_flash.new'] ?? []);
+        return $flashData[$key] ?? $default;
     }
 
     public function set(string $key, $value): void
     {
-        $_SESSION[$key] = $value;
+        $this->data[$key] = $value;
     }
 
     public function has(string $key): bool
     {
-        return isset($_SESSION[$key]);
+        if (isset($this->data[$key])) {
+            return true;
+        }
+        $flashData = array_merge($this->data['_flash.old'] ?? [], $this->data['_flash.new'] ?? []);
+        return isset($flashData[$key]);
     }
 
     public function remove(string $key): void
     {
-        unset($_SESSION[$key]);
+        unset($this->data[$key]);
     }
 
     public function clear(): void
@@ -43,7 +59,54 @@ class Session
 
     public function all(): array
     {
-        return $_SESSION;
+        return $this->data;
+    }
+
+    public function flash(string $key, mixed $value): void
+    {
+        $flashData = $this->get('_flash.new', []);
+        $flashData[$key] = $value;
+        $this->set('_flash.new', $flashData);
+    }
+
+    public function old(string $key, $default = null)
+    {
+        $old = $this->get('old', []);
+        return $old[$key] ?? $default;
+    }
+
+    public function error(string $key, $default = null)
+    {
+        $errors = $this->get('errors', []);
+        return $errors[$key] ?? $default;
+    }
+
+    protected function prepareForNextRequest(): void
+    {
+        // remove the current old flash data
+        $this->remove('_flash.old');
+
+        // get the new flash data
+        $new = $this->get('_flash.new', []);
+
+        // remove the _flash.new key
+        $this->remove('_flash.new');
+
+        // move the new flash data to the old flash data
+        $this->set('_flash.old', $new);
+
+        // write the data to the session
+        $_SESSION = $this->data;
+    }
+
+    public function __destruct()
+    {
+        $this->prepareForNextRequest();
+    }
+
+    private function migrateDataForCurrentRequest(): void
+    {
+        $this->data = $_SESSION;
     }
 
 }
