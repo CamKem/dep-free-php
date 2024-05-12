0d8db9b990128d0378e4ab3e642e1a7f5905a1b6
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-12 20:46:42 +1000

    feat(auth): Auth class created

---
+++ app/Core/Authentication/Auth.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Core\Authentication;
+
+use App\Models\User;
+use SensitiveParameter;
+
+class Auth
+{
+
+    protected ?User $user = null;
+
+    public function __construct()
+    {
+        if (session()->has('user')) {
+            $userExists = User::where('email', session()->get('user')->email)->exists();
+            if (!$userExists) {
+                session()->remove('user');
+            }
+            $this->user = User::where('email', session()->get('user')->email)->first();
+        }
+    }
+
+    public function user(): ?User
+    {
+        return $this->user ?? session()->get('user');
+    }
+
+    public function check(): bool
+    {
+        return $this->user() !== null;
+    }
+
+    public function login(User $user): void
+    {
+        $this->user = $user;
+        session()->set('user', $user);
+    }
+
+    public function attempt(#[SensitiveParameter] array $credentials): bool
+    {
+        $user = $this->getUserByEmail($credentials['email']);
+        if (!$user || !password_verify($credentials['password'], $user->password)) {
+            return false;
+        }
+
+        $this->login($user);
+
+        return true;
+    }
+
+    public function logout(): void
+    {
+        $this->user = null;
+        session()->remove('user');
+    }
+
+    public static function getUserByEmail(#[SensitiveParameter] string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+}
