eb65a16696e1877d3593b8029b85ef226cb525fc
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-18 23:44:42 +1000

    feat(middleware): add PreviousUrlMiddleware with after method to be handled after the request

---
+++ app/Middleware/PreviousUrlMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Core\Middleware;
+use App\Core\Http\Request;
+use Closure;
+use Override;
+use function session;
+
+
+class PreviousUrlMiddleware extends Middleware
+{
+    #[Override]
+    public function handle(Request $request, Closure $next): Closure
+    {
+        return $next($request);
+    }
+
+    /**
+     * Store the previous URL in the session.
+     * After the request has been handled.
+     * @see MiddlewareService::boot
+     * @uses self::after
+     */
+    public function after(Request $request): void
+    {
+        session()->set('previous.url', $request->url());
+    }
+
+}
