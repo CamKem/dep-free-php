5d454d5dc67f2a3b4edc8f7c94f6d6b0c373cb44
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-21 00:21:13 +1000

    chore: minor changes

--- templates/categories/show.view.php
+++ templates/categories/show.view.php
@@ -1,11 +1,8 @@
 <section>
     <h2><?= $category->name ?></h2>
-    <?php if (empty($products->toArray())) : ?>
+    <?php if (empty($products->toArray())): ?>
         <p class="text-center">No products found.</p>
     <?php else : ?>
-        <?= add('products.partials.grid', [
-                'products' => $products,
-                'category' => $category,
-            ]) ?>
+        <?= add('products.partials.grid', compact('products', 'category')) ?>
     <?php endif; ?>
 </section>
--- templates/users/dashboard.view.php
+++ templates/users/dashboard.view.php
@@ -1,6 +1,17 @@
 <section>
     <h2>User Dashboard</h2>
     <div class="standard-container">
+        <!-- TODO: Add link to admin if the user has the role of admin -->
         <p>Welcome, <?= $user->username; ?>!</p>
+        <p>Here are some of your recent orders:</p>
+        <ul>
+            <?php foreach ($user->orders as $order): ?>
+                <li>
+                    <a href="<?= route('orders.show', ['order' => $order->id]) ?>">
+                        Order #<?= $order->id; ?> - <?= $order->created_at->format('Y-m-d'); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </section>
