9c0a334005fbd373ef3be16d206481337249828f
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-26 16:14:38 +1000

    feat(sns): add confirmation tot he controller

--- app/Controllers/Admin/SnsController.php
+++ app/Controllers/Admin/SnsController.php
@@ -61,7 +61,7 @@ class SnsController
         // Extract relevant data from $notification and take action
     }
 
-    protected function confirmSubscription(string $subscribeUrl): void
+    protected function confirmSubscription(string $subscribeUrl)
     {
         $ch = curl_init($subscribeUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
