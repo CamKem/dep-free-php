f46189003d52e60cafe0b5840010230361b6ab32
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-27 22:52:24 +1000

    feat(checkout): class to handle progress & card details created in js

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,60 +1,160 @@
-class Checkout {
-    constructor(totalSteps, cart) {
-        this.currentStep = 1;
-        this.totalSteps = totalSteps;
-        this.cart = cart;
-        this.nextButton = document.getElementById('next-button');
-        this.form = document.getElementById('checkout-form');
-        this.circles = document.querySelectorAll('.checkout-progress .circles .circle');
-        this.orderSummary = document.querySelector('.order-summary');
+export class Progress {
+    constructor(circles, bars, prevButtons, nextButtons, sections, validator) {
+        this.circles = circles;
+        this.bars = bars;
+        this.nextButtons = nextButtons;
+        this.prevButtons = prevButtons;
+        this.sections = sections;
+        this.validator = validator;
+        this.currentStep = 0;
+        this.setUp();
     }
 
-    updateProgressBar() {
-        this.circles[this.currentStep - 1].classList.add('active');
-        if (this.currentStep > 1) {
-            this.circles[this.currentStep - 2].classList.remove('active');
-            this.circles[this.currentStep - 2].classList.add('done');
-        }
-    }
-
-    updateOrderSummary() {
-        // Update the order summary based on the current state of the cart
-        // This will depend on the structure of your cart and how you want to display the order summary
-    }
-
-    handleFormSubmission() {
-        this.form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            // Handle form submission
-            // This will depend on how you want to handle form submission
+    setUp() {
+        this.circles.forEach(function (circle) {
+            circle.className = 'circle';
         });
+
+        this.bars.forEach(function (bar) {
+            bar.className = 'bar';
+        });
+
+        this.setUpButtons();
+        this.setUpEnterHandler();
+        this.forward();
     }
 
-    handleNextButtonClick() {
-        this.nextButton.addEventListener('click', () => {
-            // Hide current step
-            document.getElementById('step' + this.currentStep).style.display = 'none';
+    setUpButtons() {
+        this.nextButtons.forEach((button, index) => {
+            if (button) {
+                if (button.tagName === 'BUTTON') {
+                    button.addEventListener('click', () => {
+                        const inputs = this.sections[index].querySelectorAll('input');
+                        let isValid = this.validator.validateFor(inputs);
+                        if (isValid) {
+                            this.sections[index].classList.add('hidden');
+                            this.sections[index + 1].classList.remove('hidden');
+                            this.forward();
+                        }
+                    });
+                }
+            }
+        });
+        this.nextButtons[0].form.addEventListener('submit', (event) => {
+            console.log('submit');
+            event.preventDefault();
+            this.forward();
+            setTimeout(() => {
+                event.target.submit();
+            }, 800);
+        });
 
-            // Show next step
-            this.currentStep++;
-            document.getElementById('step' + this.currentStep).style.display = 'block';
-
-            // Update the progress bar
-            this.updateProgressBar();
-
-            // Update the order summary
-            this.updateOrderSummary();
-
-            // If we're on the last step, change the button to a submit button
-            if (this.currentStep === this.totalSteps) {
-                this.nextButton.type = 'submit';
-                this.nextButton.textContent = 'Submit';
+        this.prevButtons.forEach((button, index) => {
+            if (button && button.tagName === 'BUTTON') {
+                button.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    this.sections[index].classList.add('hidden');
+                    this.sections[index - 1].classList.remove('hidden');
+                    this.back();
+                });
             }
         });
     }
 
-    init() {
-        this.handleNextButtonClick();
-        this.handleFormSubmission();
+    setUpEnterHandler() {
+        this.sections.forEach((section, index) => {
+            section.addEventListener('keydown', (event) => {
+                if (event.key === 'Enter') {
+                    event.preventDefault();
+                    // if the last section is reached, submit the form
+                    if (index === this.sections.length - 1) {
+                        this.forward();
+                        setTimeout(() => {
+                            this.nextButtons[0].click();
+                        }, 800);
+                    } else {
+                        this.nextButtons[index].click();
+                    }
+                }
+            });
+        });
+    }
+
+    forward() {
+        if (this.circles[this.currentStep]) {
+            this.circles[this.currentStep].classList.add('active');
+        }
+        if (this.circles[this.currentStep - 1]) {
+            this.circles[this.currentStep - 1].classList.remove('active');
+            this.circles[this.currentStep - 1].classList.add('done');
+            this.circles[this.currentStep - 1].querySelector('.label').innerHTML = '&#10003;';
+        }
+        if (this.bars[this.currentStep]) {
+            this.bars[this.currentStep].classList.add('active');
+        }
+        if (this.bars[this.currentStep - 1]) {
+            this.bars[this.currentStep - 1].classList.remove('active');
+            this.bars[this.currentStep - 1].classList.add('done');
+        }
+        if (this.currentStep === this.circles.length - 1) {
+            this.circles[this.currentStep].classList.add('active');
+        }
+        this.currentStep++;
+    }
+
+    back() {
+        if (this.currentStep > 0) {
+            this.currentStep--;
+            if (this.circles[this.currentStep]) {
+                this.circles[this.currentStep].classList.remove('done');
+                this.circles[this.currentStep].querySelector('.label').innerHTML = this.currentStep + 1;
+            }
+            if (this.bars[this.currentStep]) {
+                this.bars[this.currentStep].classList.remove('done');
+            }
+            if (this.circles[this.currentStep - 1]) {
+                this.circles[this.currentStep - 1].classList.add('active');
+            }
+        }
+    }
+}
+
+export class CardHandler {
+    constructor(inputs) {
+        this.inputs = inputs;
+        this.fullCardNumberInput = document.getElementById('card_number');
+        this.setUp();
+    }
+
+    setUp() {
+        this.inputs.forEach((input, index) => {
+            input.addEventListener('input', (event) => {
+                // Get the value of the input field and remove any whitespace
+                const value = event.target.value.replace(/\s/g, '');
+                // If the card number is less than 16 digits, return
+                if (value.length === 16) {
+                    // Split the card number into 4 segments
+                    const segments = value.match(/.{1,4}/g) || [];
+                    // Fill the input fields with the segments
+                    for (let i = 1; i <= 4; i++) {
+                        const input = document.getElementById(`card-number-${i}`);
+                        input.value = segments[i - 1] || '';
+                    }
+                }
+                if (event.target.value.length >= 4 && index < this.inputs.length - 1) {
+                    this.inputs[index + 1].focus();
+                    this.inputs[index + 1].select();
+                }
+            });
+            input.addEventListener('focus', () => {
+                input.select();
+            });
+        });
+
+        this.inputs.forEach(input => {
+            input.addEventListener('input', () => {
+                this.fullCardNumberInput.value = this.inputs.map(input => input.value).join('');
+            });
+        });
     }
 }
