5db13d73e6d2c9ae4e841f7f9b8a505b8a423687
Cam Kemshal-Bell <cam@iterated.tech>
2024-05-29 23:57:14 +1000

    feat(relations): implement base relation class

--- app/Core/Database/Relations/HasManyThrough.php
+++ app/Core/Database/Relations/HasManyThrough.php
@@ -4,51 +4,70 @@ namespace app\Core\Database\Relations;
 
 use app\Core\Database\Model;
 use app\Core\Database\QueryBuilder;
+use app\Core\Database\Relation;
+use Override;
 
-class HasManyThrough
+class HasManyThrough extends Relation
 {
 
     public function __construct(
-        protected Model $parent,
-        protected Model $related,
-        protected string $pivotTable,
+        protected Model  $parent,
+        protected Model  $related,
+        protected Model  $pivot,
         protected string $foreignKey,
-        protected string $relatedKey)
-    {}
-
-    public function getRelatedTable(): string
+        protected string $relatedKey
+    )
     {
-        return $this->related->getTable();
     }
 
-    public function getPivotTable(): string
+    #[Override]
+    public function getParentTable(): string
     {
-        return $this->pivotTable;
+        return $this->parent->getTable();
     }
 
+    #[Override]
     public function getForeignKey(): string
     {
         return $this->foreignKey;
     }
 
+    #[Override]
+    public function getRelatedTable(): string
+    {
+        return $this->related->getTable();
+    }
+
+    #[Override]
     public function getRelatedKey(): string
     {
         return $this->relatedKey;
     }
 
-    public function query(): QueryBuilder
+    public function getPivotTable(): string
     {
-        return $this->related->query()->where('id', 'IN', function ($query) {
-            $query->select($this->getRelatedKey())->from($this->getPivotTable())->where($this->getForeignKey(), $this->parent->id);
-        });
+        return $this->pivot->getTable();
     }
 
-    // TODO: get this working
-
-    // implement __toString() to return the SQL query
-    public function __toString(): string
+    public function getParentId(): int
     {
-        return "SELECT * FROM {$this->getRelatedTable()} WHERE id IN (SELECT {$this->getRelatedKey()} FROM {$this->getPivotTable()} WHERE {$this->getForeignKey()} = {$this->parent->id})";
+        return $this->parent->id;
+    }
+
+    #[Override]
+    public function query(): QueryBuilder
+    {
+        return $this->related->query()
+            ->setRelation($this)
+            ->select("{$this->getRelatedTable()}.*")
+            ->join("{$this->pivot->getTable()} AS pivot", "pivot.{$this->getRelatedKey()}", "=", "id")
+            ->join("{$this->getParentTable()} AS origin", "origin.id", "=", "pivot.{$this->getForeignKey()}")
+            ->where("origin.id", "=", $this->getParentId());
+    }
+
+    public function getRelationName(): string
+    {
+        return strtolower(class_basename($this->related)) . 's';
     }
 
 }
