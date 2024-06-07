79eac856cb9c54e7a1b90acbd10a84831e96a6e1
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-07 18:34:38 +1000

    feat(dashboard): added in better styling

--- templates/users/dashboard.view.php
+++ templates/users/dashboard.view.php
@@ -1,17 +1,59 @@
+<script type="module">
+    import RemoveManager from '/scripts/remove.js';
+
+    new RemoveManager('delete-form');
+</script>
+<?= add('modals.confirmation', ['action' => 'delete']) ?>
 <section>
     <h2>User Dashboard</h2>
     <div class="standard-container">
-        <!-- TODO: Add link to admin if the user has the role of admin -->
-        <p>Welcome, <?= $user->username; ?>!</p>
-        <p>Here are some of your recent orders:</p>
-        <ul>
-            <?php foreach ($user->orders as $order): ?>
-                <li>
-                    <a href="<?= route('orders.show', ['order' => $order->id]) ?>">
-                        Order #<?= $order->id; ?> - <?= $order->created_at->format('Y-m-d'); ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <div class="mb-8 form-bottom dashboard-header">
+            <p>Welcome, <?= $user->username; ?>!</p>
+            <?php if ($user->isAdmin()): ?>
+                <p>You have admin privileges, visit
+                    <a class="general-text" style="color: var(--blue-color);"
+                       href="<?= route('admin.index') ?>"> Admin Panel</a></p>
+            <?php endif; ?>
+        </div>
+        <?php if ($orders->isEmpty()): ?>
+            <p class="text-section">You have no orders yet.</p>
+        <?php else: ?>
+            <ul class="orders">
+                <p class="order-heading">Here are some of your recent orders:</p>
+                <?php $count = 0; ?>
+                <?php foreach ($orders->toArray() as $index => $order): ?>
+                    <?php $count++; ?>
+                    <li class="order-item-grid <?= $count % 2 === 0 ?: 'bg-light-grey' ?>">
+                        <div class="flex-center align-start">
+                            <a class="order-link"
+                               href="<?= route('orders.show', ['order' => $order['id']]) ?>">
+                                Order #<?= $order['id']; ?>
+                                - <?= date_format(date_create($order['created_at']), 'd/m/Y'); ?>
+                            </a>
+                        </div>
+                        <div class="status">
+                            <?= $order['status']; ?>
+                        </div>
+                        <div class="order-buttons">
+                            <button class="desktop-only">
+                                <a href="<?= route('orders.show', ['order' => $order['id']]) ?>">
+                                    Track Order
+                                </a>
+                            </button>
+                            <form method="post"
+                                  id="delete-form"
+                                  name="delete-form"
+                                  action="<?= route('orders.destroy', ['order' => $order['id']]) ?>">
+                                <input type="hidden" name="_method"
+                                       value="DELETE">
+                                <button type="submit" class="delete-button">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
     </div>
 </section>
