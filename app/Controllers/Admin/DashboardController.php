7789faf2f77028e16b7678bfa606d3014cc81e1f
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-10 17:02:22 +1000

    feat(admin): add bento squares for showing the stats on the admin dashboard

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers\Admin;
 
 use App\Core\Template;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 
 class DashboardController
 {
@@ -11,6 +14,9 @@ class DashboardController
     {
         return view('admin.dashboard', [
             'title' => 'Dashboard',
+            'users' => (new User)->query()->get(),
+            'orders' => (new Order)->query()->get(),
+            'products' => (new Product)->query()->get(),
         ]);
     }
 
--- templates/admin/dashboard.view.php
+++ templates/admin/dashboard.view.php
@@ -1,1 +1,19 @@
-<?php
+<div class="dashboard">
+    <div class="statistic">
+        <h3>Users</h3>
+        <p id="userCount"><?= $users->count() ?></p>
+    </div>
+    <div class="statistic">
+        <h3>Orders</h3>
+        <p id="orderCount"><?= $orders->count() ?></p>
+    </div>
+    <div class="statistic">
+        <h3>Products</h3>
+        <p id="productCount"><?= $products->count() ?></p>
+    </div>
+    <div class="statistic">
+        <h3>Categories</h3>
+        <p id="categoryCount"><?= session()->get('categories')->count() ?></p>
+    </div>
+    <!-- Add more statistics here -->
+</div>
