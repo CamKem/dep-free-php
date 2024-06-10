ea94a09f5c4c56272cd5df3cec54918b5760274e
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-10 18:53:09 +1000

    feat(admin): extract header from the admin layout view

--- templates/layouts/admin.view.php
+++ templates/layouts/admin.view.php
@@ -1,24 +1,5 @@
 <?= add('layouts.admin.head', ['title' => $title]) ?>
-<div class="admin-header">
-    <div class="logo">
-        <a href="/" aria-label="Sports Warehouse Home" class="logo">
-            <img src="/images/sports-warehouse-logo.svg"
-                 alt="Sports Warehouse Logo">
-        </a>
-    </div>
-    <script type="module">
-        import { MenuToggle } from '/scripts/menuToggle.js';
-        window.onload = () => {
-            new MenuToggle('menu-icon', 'menu-nav');
-        }
-    </script>
-    <div class="menu-icon-container">
-        <a id="menu-icon">
-            <i class="fas fa-bars" aria-hidden="true"></i>
-        </a>
-    </div>
-    <?= add('layouts.admin.menu-nav') ?>
-</div>
+<?= add('layouts.admin.header') ?>
 
 <div class="admin-breadcrumbs">
     <div class="breadcrumbs">
