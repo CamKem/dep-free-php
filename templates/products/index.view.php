253f68a2397a798f9d9d4cb6e311ce8f8b06721f
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-10 16:45:06 +1000

    feat(products): updated styling on products search

--- templates/products/index.view.php
+++ templates/products/index.view.php
@@ -1,7 +1,7 @@
 <section>
     <h2>Products</h2>
-    <?php if (empty($products->toArray())) : ?>
-        <p class="text-center">No products found.</p>
+    <?php if ($products->isEmpty()) : ?>
+        <p class="text-section">No products found.</p>
     <?php else : ?>
         <?= add('products.partials.grid', ['products' => $products]) ?>
     <?php endif; ?>
