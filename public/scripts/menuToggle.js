f20a0af2205095eabf045fc322b2774b76b8cd59
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-10 15:44:36 +1000

    feat(admin): added a generic toggle menu class.

--- public/scripts/menuToggle.js
+++ public/scripts/menuToggle.js
@@ -1,4 +1,4 @@
-export default function toggleMobileMenu() {
+export function toggleMobileMenu() {
     const mobileNav = document.querySelector('.mobile-nav');
     const toggle = document.querySelector('.menu-icon');
     const menuIcon = document.querySelector('.menu-icon > i');
@@ -22,4 +22,31 @@ export default function toggleMobileMenu() {
                 break;
         }
     });
+}
+
+export class MenuToggle {
+constructor(toggle, menu) {
+        this.menu = document.getElementById(menu);
+        this.toggle = document.getElementById(toggle);
+        this.icon = this.toggle.querySelector('i');
+        this.toggleMenu();
+    }
+
+    toggleMenu() {
+        this.toggle.addEventListener('click', (event) => {
+            event.preventDefault();
+            switch (this.menu.classList.contains('hidden')) {
+                case true:
+                    this.icon.classList.remove('fa-bars');
+                    this.icon.classList.add('fa-times');
+                    this.menu.classList.remove('hidden');
+                    break;
+                case false:
+                    this.icon.classList.remove('fa-times');
+                    this.icon.classList.add('fa-bars');
+                    this.menu.classList.add('hidden');
+                    break;
+            }
+        });
+    }
 }
