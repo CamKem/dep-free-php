ceb25d3dd6bf94cf6055a50fa46bd70bfa5d5f21
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-11 19:52:43 +1000

    feat(admin): users update is working correctly

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -43,7 +43,68 @@ class UserController
 
     public function update(Request $request): Response
     {
-        dd($request->all());
+        $user = (new User())
+            ->query()
+            ->find($request->get('id'))
+            ->first();
+
+        if (!$user) {
+            session()->flash('flash-message', 'Error: User not found.');
+            return response()->back();
+        }
+
+        if ($request->has('remove_role') === true) {
+            $user->roles()->detach(['role_id' => $request->get('role_id')]);
+            session()->flash('flash-message', 'Role removed successfully.');
+            return response()->redirect(route('admin.users.show', ['id' => $request->get('id')]));
+        }
+
+        // validate and update the users details
+        $validated = (new Validator())->validate(
+            $request->only(['username', 'email', 'roles']),
+            [
+                'username' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'email', 'max:255'],
+                'roles' => ['array'],
+            ]
+        );
+
+        if (!$validated) {
+            session()->flash('flash-message', 'Error: Please check the form for errors.');
+            return response()->back()
+                ->withErrors($validated)
+                ->withInput($request->all());
+        }
+
+        $user->query()->update([
+            'username' => $validated['username'],
+            'email' => $validated['email'],
+        ])->save();
+
+        // if request has password hash it and then update the user
+        if ($request->has('password')) {
+            $user->query()->update([
+                'password' => password_hash($request->get('password'), PASSWORD_DEFAULT),
+            ])->save();
+        }
+
+        // get the user again
+        $user = (new User())
+            ->query()
+            ->find($request->get('id'))
+            ->first();
+
+        // make an array with the roles using the role id as the key
+        $roles = [];
+        foreach ($validated['roles'] as $role) {
+            $roles[$role] = ['role_id' => $role];
+        }
+
+        // sync the roles
+        $user->roles()->sync($roles);
+
+        session()->flash('flash-message', 'User updated successfully.');
+        return response()->redirect(route('admin.users.show', ['id' => $request->get('id')]));
     }
 
     public function store(Request $request): Response
