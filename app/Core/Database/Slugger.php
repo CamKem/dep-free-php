d423201c9fa1b1d19978dad84668ffe472ac94a4
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-12 10:33:17 +1000

    feat(database): slugger class created to handle slugs and unique slugs

---
+++ app/Core/Database/Slugger.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\Core\Database;
+
+class Slugger
+{
+
+    public static function slugify(string $string): string
+    {
+        return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string), '-'));
+    }
+
+    public static function uniqueSlug(string $string, string $model, string $column): string
+    {
+        $slug = static::slugify($string);
+        $count = 1;
+        while (static::slugExists(new $model, $column, $slug)) {
+            $slug = static::slugify($string) . '-' . $count++;
+        }
+        return $slug;
+    }
+
+    private static function slugExists(Model $model, string $column, string $slug): bool
+    {
+        $query = (new QueryBuilder($model))
+            ->select('count(*) as count')
+            ->where($column, '=', $slug)
+            ->getRaw();
+        return $query[0]['count'] > 0;
+    }
+
+}
