027f3150ba3da69ffc27ddfc21574e1ac6f99406
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-14 12:17:00 +1000

    feat(products): added index method to display products on the admin panel

--- app/Controllers/Admin/ProductController.php
+++ app/Controllers/Admin/ProductController.php
@@ -3,14 +3,31 @@
 namespace App\Controllers\Admin;
 
 use App\Core\Template;
+use App\Models\Category;
+use App\Models\Product;
 
 class ProductController
 {
 
-    public function index(): Template
+    public function index(Request $request): Template
     {
+        $products = (new Product())->query()
+            ->with('category')
+            ->withCount('orders')
+            ->orderBy('created_at', 'desc');
+
+        if ($request->has('search')) {
+            $products->where('name', 'like', "%{$request->get('search')}%")
+                ->orWhere('description', 'like', "%{$request->get('search')}%");
+        }
+
         return view('admin.products.index', [
             'title' => 'products',
+            'title' => 'Manage Products',
+            'products' => $products->paginate(5),
+            'categories' => (new Category())->query()->get(),
+        ]);
+    }
         ]);
     }
 
