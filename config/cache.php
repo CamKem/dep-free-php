4a302276e9f1f3707c89766ca4b7334d12ff6940
Cam Kemshal-Bell <cam@iterated.tech>
2024-06-22 15:28:27 +1000

    feat(cache): cache config holding the values

--- config/cache.php
+++ config/cache.php
@@ -6,13 +6,13 @@ configure(fn() => [
     'driver' => env('CACHE_DRIVER', 'file'),
     'file' => [
         'path' => BASE_PATH . 'storage/cache',
-        'lifetime' => 3600,
-        'prefix' => 'cache_',
+        'lifetime' => env('CACHE_LIFETIME', 3600),
+        'prefix' => env('CACHE_PREFIX', 'cache_'),
         'extension' => '.cache'
     ],
     'database' => [
         'table' => 'cache',
-        'lifetime' => 3600,
-        'prefix' => 'cache_',
+        'lifetime' => env('CACHE_LIFETIME', 3600),
+        'prefix' => env('CACHE_PREFIX', 'cache_'),
     ],
 ]);
