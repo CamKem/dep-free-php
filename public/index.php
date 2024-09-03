d35b1a52d21cc06699838a4626ee59d0c64f9a71
Cam Kemshal-Bell <cam@iterated.tech>
2024-09-03 18:24:23 +1000

    fix: namespace correlate

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ const BASE_PATH = __DIR__.'/../';
 include BASE_PATH . 'bootstrap/functions.php';
 
 spl_autoload_register(static function ($class) {
-    $class = preg_replace('/^App/', 'app', $class);
+    $class = preg_replace('/^App\\\\/', 'app/', $class);
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     $file = BASE_PATH . "{$class}.php";
@@ -14,7 +14,6 @@ spl_autoload_register(static function ($class) {
     } else {
         $error = "Autoload Error: Unable to load class: {$class} from file {$file}";
         error_log($error);
-        throw new \RuntimeException($error);
     }
 });
 
