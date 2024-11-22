56d791ffd00d99e7dbf037aab64826853d1de3e6
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-22 20:40:05 +1100

    feat(search): implement basic fuzzy search algo with levenshtein

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,17 +12,31 @@ class ProductController extends Controller
 
     public function index(Request $request): Template
     {
-        $query = (new Product())
+        $products = (new Product())
             ->query()
-            ->with('category');
+            ->with('category')
+            ->orderBy('name')
+            ->get();
 
-        if ($request->has('search')){
-            $query->where('name', 'like', "%{$request->get('search')}%");
+        if ($request->has('search')) {
+            $search = $request->get('search');
+
+            $filteredProducts = $products
+                ->each(fn($product) => $product->lev = levenshtein($product->name, $search))
+                ->sortBy('lev');
+
+            // TODO: write a better fuzzy search algorithm
+            //  the smaller the min levenshtein distance, the closer the match
+            //  so the less impact the threshold has & the more strict the match should be so it's included
+            $minLev = $products->min('lev');
+            $threshold = $minLev + (strlen($search) / 2);
+            $products = $filteredProducts
+                ->filter(fn($product) => $product->lev <= $threshold);
         }
 
         return view("products.index", [
             'title' => 'Products',
-            'products' => $query->get(),
+            'products' => $products,
         ]);
     }
 
