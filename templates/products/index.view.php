7ba0efd43aabe30dad36a8f8e5f9797e4beec938
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-24 17:03:05 +1100

    feat: show "did you mean" message for incorrectly matched words

--- templates/products/index.view.php
+++ templates/products/index.view.php
@@ -1,5 +1,10 @@
 <section>
     <h2>Products</h2>
+    <?php if ($shouldShowDidYouMean) : ?>
+        <div class="standard-container mb-8">
+            <p class="text-section">Did you mean <a href="<?= route('products.index', ['search' => $matched_product_words]) ?>" class="general-link"><?= implode(' ,', $matched_product_words) ?></a> instead of <strong class="general-link"><?= implode(' ,', $matched_search_words) ?></strong>?</p>
+        </div>
+    <?php endif; ?>
     <?php if ($products->isEmpty()) : ?>
         <div class="standard-container">
             <p class="text-section">No products found.</p>
