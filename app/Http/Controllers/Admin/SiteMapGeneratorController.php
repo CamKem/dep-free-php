70960a8d84cd14971199eb56aba924c173573e83
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-24 21:41:15 +1100

    feat: invokable controller to generate sitemap

---
+++ app/Http/Controllers/Admin/SiteMapGeneratorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Core\FileSystem\Storage;
+use App\Core\Routing\Route;
+use App\Core\Routing\Router;
+
+class SiteMapGeneratorController
+{
+    public function __invoke()
+    {
+        $routes = app()->resolve(Router::class)->getRoutes();
+        $sitemap = $this->generateSiteMap($routes);
+        $updated = $this->updateSiteMapFile($sitemap);
+        if ($updated !== false) {
+            return response()->back()->with('success', 'Site map generated successfully.');
+        }
+    }
+
+    private function generateSiteMap($routes): string
+    {
+        $sitemap = '<?xml version="1.0" encoding="UTF-8" ?>' . PHP_EOL;
+        $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
+        /** @var Route $route */
+        foreach ($routes as $route) {
+            if ($route->getMethod() === 'GET'
+                && !str_contains($route->uri(), 'admin')
+                && in_array('admin', $route->getMiddleware(), true) === false
+            ) {
+                $sitemap .= '    <url>' . PHP_EOL;
+                $sitemap .= '        <loc>' . url($route->uri()) . '</loc>' . PHP_EOL;
+                $sitemap .= '        <lastmod>' . date('Y-m-d') . '</lastmod>' . PHP_EOL;
+                $sitemap .= '        <changefreq>daily</changefreq>' . PHP_EOL;
+                $sitemap .= '        <priority>0.8</priority>' . PHP_EOL;
+                $sitemap .= '    </url>' . PHP_EOL;
+            }
+        }
+
+        $sitemap .= '</urlset>' . PHP_EOL;
+
+        return $sitemap;
+    }
+
+    private function updateSiteMapFile($sitemap): string|false
+    {
+        $filePath = public_path('sitemap.xml');
+        return (new Storage($filePath))->
+            put($filePath, $sitemap);
+    }
+}
