d2f79f1f8b252c0d8ec564f8a371930a3bce9637
Cam Kemshal-Bell <cam@iterated.tech>
2024-11-24 20:55:28 +1100

    feat: handle cases of multiple matched words to each search phase

--- templates/products/index.view.php
+++ templates/products/index.view.php
@@ -2,7 +2,21 @@
     <h2>Products</h2>
     <?php if ($shouldShowDidYouMean) : ?>
         <div class="standard-container mb-8">
-            <p class="text-section">Did you mean <a href="<?= route('products.index', ['search' => $matched_product_words]) ?>" class="general-link"><?= implode(' ,', $matched_product_words) ?></a> instead of <strong class="general-link"><?= implode(' ,', $matched_search_words) ?></strong>?</p>
+            <p class="text-section">Did you mean
+                <?php if (count($matched_product_words) === 1) : ?>
+                    <a href="<?= route('products.index', ['search' => $matched_product_words]) ?>" class="general-link"><?= $matched_product_words[0] ?></a> instead of <strong class="general-link"><?= $matched_search_words[0] ?></strong>?</p>
+                <?php else : ?>
+                        <?php foreach ($matched_product_words as $index => $matched_product_word) : ?>
+                            <a href="<?= route('products.index', ['search' => $matched_product_word]) ?>" class="general-link"><?= $matched_product_word ?></a>
+                            <?php if ($index < count($matched_product_words) - 1) : ?>
+                                or,
+                            <?php else : ?>
+                                instead of <strong class="general-link"><?= implode(', ', $matched_search_words) ?></strong>?
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+
+                <?php endif; ?>
+            </p>
         </div>
     <?php endif; ?>
     <?php if ($products->isEmpty()) : ?>
