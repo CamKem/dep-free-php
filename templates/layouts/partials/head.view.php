2372d89cf82ad8be6aac427614d70fcceafd3146
Cam Kemshal-Bell <cam@iterated.tech>
2024-12-19 22:01:13 +1100

    chore: update meta

--- templates/layouts/partials/head.view.php
+++ templates/layouts/partials/head.view.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Sports warehouse is a sports store that sells sports equipment and sportswear.">
+    <meta name="description" content="Family owned sports store that sells sports equipment and sportswear.">
     <meta name="keywords" content="sports, equipment, sportswear, sports store">
-    <meta name="author" content="Sports Warehouse">
+    <meta name="author" content="Cameron Kemshal-Bell">
     <meta name="robots" content="index, follow">
     <meta property="og:title" content="<?= config('app.name') . ' - ' . $title ?>">
-    <meta property="og:description" content="Sports warehouse is a sports store that sells sports equipment and sportswear.">
+    <meta property="og:description" content="Family owned sports store that sells sports equipment and sportswear.">
     <meta property="og:image" content="<?= config('app.url') ?>/images/sports-warehouse-logo.svg">
     <meta property="og:url" content="<?= config('app.url') ?>">
     <meta property="og:type" content="website">
-    <meta property="og:site_name" content="Sports Warehouse">
+    <meta property="og:site_name" content="<?= config('app.name') ?>">
     <meta property="og:locale" content="en_AU">
     <title><?= config('app.name') . ' - ' . $title ?></title>
     <link rel="stylesheet" href="/styles/main.css">
